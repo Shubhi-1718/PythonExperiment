73522363cbb2a2ec75d1619b7c5239f5c649bd98
Shubhangini Agrawal <119943709+Shubhi-1718@users.noreply.github.com>
2023-09-11 12:23:34 +0530

    Update WaterJug.py

--- WaterJug.py
+++ WaterJug.py
@@ -1,7 +1,7 @@
-x=0
-y=0
-m=4
-n=3
+jug1=0
+jug2=0
+jug1cap=4
+jug2cap=3
 print("initial state=(0,0)")
 print ("Rule 1 : Fill the 4 gallon jug ")
 print ("Rule 2: Fill the 3 gallon jug")
@@ -11,36 +11,35 @@ print ("Rule 5: Pour some water from 3 gallon jug to fill the 4 gallon jug")
 print ("Rule 6: Pour some water from 4 gallon jug to 3 gallon jug")
 print ("Rule 7: Pour all the water from 3 gallon jug to 4 gallon jug")
 print ("Rule 8: Pour all the water from 4 gallon jug to 3 gallon jug")
-while x!=2:
+while jug1!=2:
     r=int(input("Enter rule"))    
     if r==1 :
         print ("Rule 1 : Fill the 4 gallon jug ") 
-        x=m
+        jug1=jug1cap
     elif r==2 :
         print ("Rule 2: Fill the 3 gallon jug")
-        y=n
+        jug2=jug2cap
     elif r==3 :
         print ("Rule 3: Empty the 4 gallon jug") 
-        x=0
+        jug1=0
     elif r==4 :
         print ("Rule 3: Empty the 4 gallon jug")
-        y=0
+        jug2=0
     elif r==5 :
         print ("Rule 5: Pour some water from 3 gallon jug to fill the 4 gallon jug")
-        t= n-y
-        y=n
-        x-=t    
+        jug1=jug1-(jug2cap-jug2)
+        jug2=jug2cap
     elif r==6:
         print ("Rule 6: Pour some water from 4 gallon jug to 3 gallon jug")
-        t= m-x
-        x=m
-        y -=t
+        jug2=jug2-(jug1cap-jug1)
+        jug1=jug1cap
     elif r==7:
         print ("Rule 7: Pour all the water from 3 gallon jug to 4 gallon jug")
-        y=y+x
-        x=0
+        jug2=jug2+jug1
+        jug1=0
     elif r==8:
         print ("Rule 8: Pour all the water from 4 gallon jug to 3 gallon jug")
-        x=x+y
-        y=0
-    print (x,y)
+        jug1=jug1+jug2
+        jug2=0
+    print (jug1,jug2)
+
